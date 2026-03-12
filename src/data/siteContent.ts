89d7e6889ec7ba9f0a9b7f6cd76ca4c62ce9ee67
Angel castaneda <apps.desarrollo3@fconfia.com>
2026-03-12 14:34:19 -0600

    Initial commit: Redesign and content update

--- src/data/siteContent.ts
+++ src/data/siteContent.ts
@@ -156,17 +156,92 @@ export const portfolioCategories = [
   { slug: "multi-cam", label: "Multi-cam" }
 ];
 
-export const managementProfiles = [
-  { slug: "sheco", label: "Sheco" },
-  { slug: "nebasa", label: "Nebasa" },
-  { slug: "jonter", label: "Jonter" },
-  { slug: "a-zar", label: "A-ZAR" },
-  { slug: "alex-zapata", label: "Alex Zapata" }
+export interface ArtistProfile {
+  slug: string;
+  label: string;
+  bio: string;
+  image: string;
+  tags?: string[];
+  socials?: { label: string; url: string }[];
+}
+
+export const managementProfiles: ArtistProfile[] = [
+  { 
+    slug: "sheco", 
+    label: "Sheco",
+    bio: "Sergio Ballesteros better known as Sheco, was introduced in the electronic music industry, being identified by his unique sound and essence in the Progressive House & Techno.",
+    image: "/images/artists/sheco.png",
+    tags: ["Progressive House", "Techno"],
+    socials: [
+      { label: "Spotify", url: "https://open.spotify.com/artist/0HVhaviIJ5Kul6EwtWrbGm?si=fKxv5tqlTy-J1an3T4eP2w" },
+      { label: "Soundcloud", url: "https://soundcloud.com/shecomusic" },
+      { label: "Facebook", url: "https://www.facebook.com/ShecoOfficial/" },
+      { label: "Instagram", url: "https://www.instagram.com/shecomusic/" }
+    ]
+  },
+  { 
+    slug: "nebasa", 
+    label: "Nebasa",
+    bio: "Nebasa is an artist and producer from Mexico City who has captivated the national and international emerging scene with his style that blends melodic electronic music genres with Hyperpop, Indie Rock, and R&B. Starting his project in 2020, Nebasa has performed at national venues such as Bajo Circuito and Hookah Condesa, as well as international events like 6th Saint Sense in Guatemala. He has also been featured as a special guest at festivals such as EDC Mexico (2023) and Mad House Pepsi Center (2022).",
+    image: "/images/artists/nebasa.png",
+    tags: ["Hyperpop", "Indie Rock", "R&B", "Melodic Electronic"],
+    socials: [
+      { label: "Spotify", url: "https://open.spotify.com/intl-es/artist/6C8sFdGAgfgSiInJ8eI3EE" },
+      { label: "Soundcloud", url: "https://soundcloud.com/nebasa" },
+      { label: "Facebook", url: "https://www.facebook.com/nebasa.music" },
+      { label: "Youtube", url: "https://www.youtube.com/channel/UC2GjAJ9iuL5AMwRfKlRhUBw" },
+      { label: "Instagram", url: "https://www.instagram.com/nebasa.music/" },
+      { label: "Twitter", url: "https://twitter.com/nebasa_music" },
+      { label: "TikTok", url: "https://www.tiktok.com/@nebasa.music" }
+    ]
+  },
+  { 
+    slug: "jonter", 
+    label: "Jonter",
+    bio: "Jonter is a 23-year-old DJ and producer from New York City, known for his innovative approach to the Dubstep and Riddim genres. Jonter started to gain recognition in the dubstep scene with support from notable acts like ATLIENS, Excision, JEANIE, Jessica Audiffred, Louiejaxx, Riot Ten and TRVCY.",
+    image: "/images/artists/jonter.png",
+    tags: ["Dubstep", "Riddim"],
+    socials: [
+      { label: "Spotify", url: "https://open.spotify.com/artist/7FdaG4SH0YSlPMKhp2UVZz?si=2BxkvyKCQtOPCjgVYJyTPA" },
+      { label: "Soundcloud", url: "https://soundcloud.com/jonterwav" },
+      { label: "Facebook", url: "https://www.facebook.com/JEFFSUAOFFICIAL" },
+      { label: "Instagram", url: "https://www.instagram.com/jontermusic/" }
+    ]
+  },
+  { 
+    slug: "a-zar", 
+    label: "A-ZAR",
+    bio: "Aldo Sarabia better known as A-ZAR, is a rising producer, born in Toluca, state capital of the State of Mexico.",
+    image: "/images/artists/a-zar.png",
+    tags: ["Producer"],
+    socials: [
+      { label: "Spotify", url: "https://open.spotify.com/artist/15YaHwQxIC59q8Y4kHbI46?si=PM8NKneVSey-lF-6rtEx-Q" },
+      { label: "Soundcloud", url: "https://soundcloud.com/a-zarmusic" },
+      { label: "Facebook", url: "https://www.facebook.com/AzarMusic" },
+      { label: "Instagram", url: "https://www.instagram.com/a.zarmusic/" },
+      { label: "Twitter", url: "https://twitter.com/azarmusic_" }
+    ]
+  },
+  { 
+    slug: "alex-zapata", 
+    label: "Alex Zapata",
+    bio: "Alejandro Zapata, better known as Alex Zapata, is a DJ, Producer, Musician, Composer, and Pianist at just 22 years old who in over 2 years, has managed to establish himself as an emerging artist from Mexico City. Alex Zapata's sound is constantly evolving and versatile, characterized by his skillful use of vocals, latin sounds, synthesized elements, real instruments, all under an exquisite rhythmic base and bassline.",
+    image: "/images/artists/alex-zapata.png",
+    tags: ["DJ", "Producer", "Musician", "Composer"],
+    socials: [
+      { label: "Spotify", url: "https://open.spotify.com/artist/4843zV3Deug2mVtdNLLsyg?si=qgpolFZ3TXGKIlGdGl_S-g" },
+      { label: "Soundcloud", url: "https://soundcloud.com/alex-zapata-24923260" },
+      { label: "Instagram", url: "https://www.instagram.com/alexzapataaa/" },
+      { label: "Twitter", url: "https://twitter.com/alexzapaaata" },
+      { label: "TikTok", url: "https://www.tiktok.com/@alexzapatamusic" }
+    ]
+  }
 ];
 
 export const contacts = [
   { name: "Daniel Montiel", email: "daniel@dne.media" },
-  { name: "Enrique Ramirez", email: "enrique@dne.media" }
+  { name: "Enrique Ramirez", email: "enrique@dne.media" },
+  { name: "Angel", email: "angel@dne.media", role: "System Administrator" }
 ];
 
 export const socialLinks = [
